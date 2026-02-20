ff5bfb04402b1326c596be27ef1baf788692ae4f
roycoayam34-commits <roycoayam34@gmail.com>
2026-02-20 16:23:02 +0700

    Implement Telegram bot with commands and conversation flow

---
+++ main.py
@@ -0,0 +1,37 @@
+import os from telegram import Update from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, ConversationHandler, ContextTypes, filters
+
+TOKEN = os.getenv("BOT_TOKEN")
+
+========= /info ========= 
+
+async def info(update: Update, context: ContextTypes.DEFAULT_TYPE): await update.message.reply_text( "Halo 👋\n\n" "Selamat datang di bot layanan kami.\n" "Gunakan /pricelist untuk melihat harga.\n" "Gunakan /isi_form untuk order." )
+
+========= /pricelist ========= 
+
+async def pricelist(update: Update, context: ContextTypes.DEFAULT_TYPE): await update.message.reply_photo(photo=open('pricelist.jpg','rb'), caption="Ini daftar harga kami 📋")
+
+========= FORM ========= 
+
+NAMA, ALAMAT, PESANAN = range(3)
+
+async def isi_form(update: Update, context: ContextTypes.DEFAULT_TYPE): await update.message.reply_text("Masukkan nama:") return NAMA
+
+async def nama(update: Update, context: ContextTypes.DEFAULT_TYPE): context.user_data["nama"] = update.message.text await update.message.reply_text("Masukkan alamat:") return ALAMAT
+
+async def alamat(update: Update, context: ContextTypes.DEFAULT_TYPE): context.user_data["alamat"] = update.message.text await update.message.reply_text("Pesanan kamu apa?") return PESANAN
+
+async def pesanan(update: Update, context: ContextTypes.DEFAULT_TYPE): context.user_data["pesanan"] = update.message.text
+
+hasil = f""" 
+
+📩 ORDER MASUK
+
+Nama: {context.user_data['nama']} Alamat: {context.user_data['alamat']} Pesanan: {context.user_data['pesanan']} """
+
+await context.bot.send_message(chat_id=6902689922, text=hasil) await update.message.reply_text("Pesanan sudah kami terima ✅") return ConversationHandler.END ========= MAIN ========= 
+
+def main(): app = ApplicationBuilder().token(TOKEN).build()
+
+app.add_handler(CommandHandler("info", info)) app.add_handler(CommandHandler("pricelist", pricelist)) conv_handler = ConversationHandler( entry_points=[CommandHandler("isi_form", isi_form)], states={ NAMA: [MessageHandler(filters.TEXT & ~filters.COMMAND, nama)], ALAMAT: [MessageHandler(filters.TEXT & ~filters.COMMAND, alamat)], PESANAN: [MessageHandler(filters.TEXT & ~filters.COMMAND, pesanan)], }, fallbacks=[], ) app.add_handler(conv_handler) print("BOT ONLINE") app.run_polling() 
+
+if name == "main": main()
